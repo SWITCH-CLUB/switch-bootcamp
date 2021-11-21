9e4de90c604ba3e3651b31f2ed3e0c863236b68c
Praveen Kumar <praveenscience@gmail.com>
2021-11-21 13:30:30 +0000

    Created the Badge Component dynamically.

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,5 +1,27 @@
-const Badge = () => {
-  return <div className="Badge">Badge gets developed here... :)</div>;
+const Badge = ({ Avatar, Username, FullName, Description }) => {
+  return (
+    <div className="Badge">
+      <div className="BadgeContainer">
+        <img
+          src="https://i.imgur.com/uIvAIyI.png"
+          alt="Background"
+          className="BadgeBackground"
+        />
+        <div className="BadgeContent">
+          <div className="Thumbnail">
+            <img src={Avatar} alt={FullName} />
+          </div>
+          <div className="Content">
+            <h1>{FullName}</h1>
+            <h2>
+              <em>{Username}</em>
+            </h2>
+            <p>{Description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Badge;
