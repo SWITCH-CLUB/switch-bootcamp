dfc76c99acd8d16c22237f74bf30e74faf8b20b7
Praveen Kumar <praveenscience@gmail.com>
2021-11-21 13:15:44 +0000

    Added Badge and Form Components on the Home Page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import Header from "./Bootstrap/Header";
+import Badge from "./Badge";
+import Form from "./Form";
 
 class App extends Component {
   state = {};
   render() {
     return (
       <div className="App">
-        <Header dark={true} className="justify-content-center">
+        <Header dark={true} className="Header justify-content-center">
           GitHub Badge Generator
         </Header>
         <div className="container">
           <div className="row">
-            <div className="col-12 col-md-4">Left Form</div>
-            <div className="col-12 col-md-8">Right Badge</div>
+            <div className="col-12 col-md-4">
+              <Form />
+            </div>
+            <div className="col-12 col-md-8">
+              <Badge />
+            </div>
           </div>
         </div>
       </div>
