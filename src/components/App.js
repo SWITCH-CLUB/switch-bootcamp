a0b7da44d054b7415d764e6f8cffed602d2c4fa4
Praveen Kumar <praveenscience@gmail.com>
2021-11-21 13:02:10 +0000

    Added right headers with skeleton for main.

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header>SWITCH</Header>
+        <Header dark={true} className="justify-content-center">
+          GitHub Badge Generator
+        </Header>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-4">Left Form</div>
+            <div className="col-12 col-md-8">Right Badge</div>
+          </div>
+        </div>
       </div>
     );
   }
