a69a553d61ab506c18333d9055ea2f651dea890f
Praveen Kumar <praveenscience@gmail.com>
2021-11-21 13:15:18 +0000

    Form created with a connected component.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import FormGroup from "./Bootstrap/FormGroup";
+
 const Form = () => {
-  return <form>Form Here...</form>;
+  const [Username, setUsername] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormGroup
+        Id="Username"
+        Label="Username"
+        Type="text"
+        Value={Username}
+        Placeholder="Enter your GitHub Username."
+        onChange={e => {
+          setUsername(e.target.value);
+        }}
+        Desc="Enter your GitHub Username."
+      />
+      <button
+        type="submit"
+        className="btn btn-primary"
+        disabled={Username.trim().length < 2}
+      >
+        Generate Badge
+      </button>
+    </form>
+  );
 };
 
 export default Form;
